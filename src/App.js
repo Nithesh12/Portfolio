39b46119a10e6d0310a1f6b7133c64fb6a393a42
github <neethu3926@gmail.com>
2022-10-14 17:09:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './index.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faCircle,faBars} from '@fortawesome/free-solid-svg-icons'
+import { FaGithub,FaInstagram,FaTwitter } from "react-icons/fa";
+import { TypeAnimation } from 'react-type-animation';
+import { useState} from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+  const[isToggled,setIsToggled]=useState(false);
+  return(
+    <div className='Container'>
+      <nav className='navbar'>
+        <ul>
+        <p></p>
+        <li className='icons'>
+          <a href='#' className='navlist' id='firstCirc'><FontAwesomeIcon icon={faCircle} /></a>
+          <a href='#' className='navlist' id="secondCirc"><FontAwesomeIcon icon={faCircle} /></a>
+          <a href='#' className='navlist' id="thirdCirc"><FontAwesomeIcon icon={faCircle} /></a>
+        </li>
+        <div className='navAll'>
+        <a id='bar' onClick={()=>setIsToggled(!isToggled)}><FontAwesomeIcon icon={faBars}/></a>
+        <div className={
+          isToggled?"navlinks":"notExpand"
+          }>
+        <ul className='barMenu'>
+        <li>
+          <a href='#' className='navlist' id="home">Home</a>
+        </li>
+        <li>   
+          <a href='#' className='navlist'>Project</a>
+        </li>
+        <li>
+          <a href='#' className='navlist'>Resume</a>
+        </li>
+        <li>
+          <a href='#' className='navlist'>Contact</a>
+        </li>
+            </ul></div></div>
+        </ul>
+      </nav>
+      <div className='word'>
+        <h4>Hi there</h4>
+          <h1>I'm </h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I am a Web Developer
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <button><a href="mailto:nitheshb24@gmail.com">Email</a></button>
+          <button className='btn'><a href="https://www.linkedin.com/in/nithesh-b-b354841a7/">Linkedin</a></button>
+        <TypeAnimation className='anime'  
+      sequence={[
+        "HTML" , 
+        1000,
+        "CSS",
+        1000,
+        'REACT',
+        1000,
+        'C++',
+        1000,
+        'PYTHON',1000
+      ]}
+      wrapper="div"
+      cursor={true}
+      repeat={Infinity}
+      style={{ fontSize: '1em' }}
+    />
+      </div>
+      <div className='image'>
+      <img src='../cartoon3-removebg-preview.png' ></img>
+      </div>
+      <div className={isToggled?"socialNotExpand":"social"}>
+        <a href='https://github.com/Nithesh12' className='appicon'> 
+      <FaGithub />
         </a>
-      </header>
+        <a href='https://www.instagram.com/n_eethugowda/' className='appicon'>
+      <FaInstagram />
+        </a>
+        <a href='https://twitter.com/NitheshGwda' className='appicon'>
+      <FaTwitter />
+        </a>
+      </div>
     </div>
   );
 }
